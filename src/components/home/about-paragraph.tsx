36e3827796f3486219d3634535d7588bc7667d35
aujezus <augustasv16@gmail.com>
2025-10-16 12:05:11 +0300

    fix: typo in about section

--- src/components/home/about-paragraph.tsx
+++ src/components/home/about-paragraph.tsx
@@ -12,29 +12,13 @@ function AboutParagraph({ className }: React.HTMLAttributes<HTMLDivElement>) {
 
   return (
     <div className={cn("flex flex-col gap-4 opacity-80 ", className)}>
-      {!showMore && (
-        <>
-          <p>
-            I&apos;m Augustas. I build full stack websites that look good and
-            work smoothly. I love clean, animated UIs, and I&apos;m also
-            comfortable with complex backend logic to keep things fast and
-            modern.
-          </p>
-          <p>
-            I care about doing great and meaningful work. My dream is to build
-            something used by millions. If you need a passionate developer on
-            your team, let&apos;s chat and bring you idea to life.
-          </p>
-        </>
-      )}
+      <p>
+        I&apos;m Augustas. I build full stack websites that look good and work
+        smoothly. I love clean, animated UIs, and I&apos;m also comfortable with
+        complex backend logic to keep things fast and modern.
+      </p>
       {showMore && (
         <>
-          <p>
-            I&apos;m Augustas. I build full stack websites that look good and
-            work smoothly. I love clean, animated UIs, and I&apos;m also
-            comfortable with complex backend logic to keep things fast and
-            modern.
-          </p>
           <p>
             My coding journey began playing web flash games, that my grandmother
             showed me. That opened my eyes to computers and I realized that with
@@ -46,13 +30,13 @@ function AboutParagraph({ className }: React.HTMLAttributes<HTMLDivElement>) {
             fixing and modding them. My favorite is the Audi S3 8L 1.8T. I also
             like to work out and drink good coffee to keep my mind sharp.
           </p>
-          <p>
-            I care about doing great and meaningful work. My dream is to build
-            something used by millions. If you need a passionate developer on
-            your team, let&apos;s chat and bring you idea to life.
-          </p>
         </>
       )}
+      <p>
+        I care about doing great and meaningful work. My dream is to build
+        something used by millions. If you need a passionate developer on your
+        team, let&apos;s chat and bring your idea to life.
+      </p>
 
       <button
         className="flex w-fit items-center gap-1 text-sm text-muted-foreground"
